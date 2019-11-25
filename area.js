9acd23b3ccdcc2388af4d685b73d8168b487e994
Jessica Lin <JLin@ucvts.tec.nj.us>
2019-11-25 13:24:52 -0500

    initial commit

---
+++ area.js
@@ -0,0 +1,21 @@
+const readlineSync = require("readline-sync");
+
+console.log();
+do {
+  input = readlineSync.question("Enter a shape: ");
+} while (input !== ("circle" || "rectangle" || "square" || "triangle"));
+
+if (input == "circle") {
+  let radius = readlineSync.question("Enter a radius: ");
+}
+if (input == "rectangle") {
+  let length = readlineSync.question("Enter a length: ");
+  let width = readlineSync.question("Enter a width: ");
+}
+if (input == "square") {
+  let side = readlineSync.question("Enter a side: ");
+}
+if (input == "triangle") {
+  let base = readlineSync.question("Enter a base: ");
+  let height = readlineSync.question("Enter a height: ");
+}
